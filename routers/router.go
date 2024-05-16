0e3123135d9bffea35e0d5a1764fe3665e18412f
waylon <1158341873@qq.com>
2024-05-16 17:25:39 +0800

    feat: 支持插件动态数据请求功能

--- routers/router.go
+++ routers/router.go
@@ -14,6 +14,7 @@ func init() {
 	beego.Router("/bk_plugin/detail/:version", &controllers.DetailController{})
 	beego.Router("/bk_plugin/invoke/:version", &controllers.InvokeController{})
 	beego.Router("/bk_plugin/schedule/:trace_id", &controllers.ScheduleController{})
+	beego.Router("/bk_plugin/plugin_api_dispatch", &controllers.PluginApiDispatchController{})
 	if conf.IsDevMode() {
 		beego.Router("/", &controllers.DebugPannelController{})
 		beego.Router("/bk_plugin/logs/:trace_id", &controllers.LogsController{})
