e245012bc1cc9f85d49c96a4308e2a2af674ed1f
ZC-A <1483681501@qq.com>
2025-02-11 19:55:46 +0800

    feat: dispatche支持请求携带文件

--- controllers/plugin_api_dispatch.go
+++ controllers/plugin_api_dispatch.go
@@ -4,28 +4,46 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	web "github.com/beego/beego/v2/server/web"
-	log "github.com/sirupsen/logrus"
 	"io"
+	"mime/multipart"
 	"net/http"
 	"net/url"
 	"strings"
+
+	web "github.com/beego/beego/v2/server/web"
+	log "github.com/sirupsen/logrus"
 )
 
 type PluginApiDispatchController struct {
 	PluginApiController
 }
 
+type PluginApiDispatchBase struct {
+	Url      string `json:"url" form:"url"`
+	Method   string `json:"method" form:"method"`
+	Username string `json:"username" form:"username"`
+}
+
 type PluginApiDispatchParam struct {
-	Url      string          `json:"url"`
-	Method   string          `json:"method"`
-	Username string          `json:"username"`
-	Data     json.RawMessage `json:"data"`
+	PluginApiDispatchBase
+	Data json.RawMessage         `json:"data" form:"data"`
+	File []*multipart.FileHeader `form:"file"`
 }
 
 type PluginApiDispatchResponse struct {
 	*BaseResponse
 	Data interface{} `json:"data"`
+}
+
+func handleErrResponse(c *PluginApiDispatchController, err error, msg string) {
+	c.Data["json"] = &PluginApiDispatchResponse{
+		BaseResponse: &BaseResponse{
+			Result:  false,
+			Message: fmt.Sprintf("%s, %v", msg, err),
+		},
+		Data: nil,
+	}
+	c.ServeJSON()
 }
 
 func (c *PluginApiDispatchController) FindController(path string, method string) (string, bool) {
@@ -51,30 +69,49 @@ func (c *PluginApiDispatchController) FindController(path string, method string)
 
 func (c *PluginApiDispatchController) Post() {
 	var param PluginApiDispatchParam
-	if err := c.BindJSON(&param); err != nil {
-		log.Errorf("param bind error: %v\n", err)
-		c.Data["json"] = &PluginApiDispatchResponse{
-			BaseResponse: &BaseResponse{
-				Result:  false,
-				Message: fmt.Sprintf("request param bind fail, %v", err),
-			},
-			Data: nil,
-		}
-		c.ServeJSON()
-		return
-	}
+	var errMsg string
 
+	contentType := c.Ctx.Request.Header.Get("Content-Type")
+
+	if strings.HasPrefix(contentType, "multipart/form-data") {
+		// 处理multipart/form-data请求
+		if err := c.BindForm(&param); err != nil {
+			errMsg = "request param bind fail"
+			log.Errorf("%s %v\n", errMsg, err)
+			handleErrResponse(c, err, errMsg)
+			return
+		}
+
+		// 文件无法直接绑定 需要手动解析
+		if err := c.Ctx.Request.ParseMultipartForm(32 << 20); err != nil {
+			errMsg = "failed to parse multipart form"
+			log.Errorf("%s %v\n", errMsg, err)
+			handleErrResponse(c, err, errMsg)
+			return
+		}
+
+		fileHeaders := c.Ctx.Request.MultipartForm.File["file"]
+		if len(fileHeaders) == 0 {
+			log.Warn("no files uploaded")
+		}
+
+		for _, fileHeader := range fileHeaders {
+			param.File = append(param.File, fileHeader)
+		}
+
+	} else {
+		if err := c.BindJSON(&param); err != nil {
+			errMsg = "param bind error"
+			log.Errorf("%s %v\n", errMsg, err)
+			handleErrResponse(c, err, errMsg)
+			return
+		}
+	}
 	parsedURL, err := url.Parse(param.Url)
 	if err != nil {
-		log.Errorf("param.Url parse fail, %v\n", err)
-		c.Data["json"] = &PluginApiDispatchResponse{
-			BaseResponse: &BaseResponse{
-				Result:  false,
-				Message: fmt.Sprintf("Url parse fail, %v", err),
-			},
-			Data: nil,
-		}
-		c.ServeJSON()
+		errMsg = "param.Url parse fail"
+		log.Errorf("%s, %v\n", errMsg, err)
+		handleErrResponse(c, err, errMsg)
 		return
 	}
 
@@ -82,15 +119,9 @@ func (c *PluginApiDispatchController) Post() {
 	upperMethod := strings.ToUpper(param.Method)
 	_, ok := c.FindController(path, upperMethod)
 	if !ok {
-		log.Errorf("controller not found, path: %s, method: %s", path, upperMethod)
-		c.Data["json"] = &PluginApiDispatchResponse{
-			BaseResponse: &BaseResponse{
-				Result:  false,
-				Message: fmt.Sprintf("controller not found, path: %s, method: %s", path, upperMethod),
-			},
-			Data: nil,
-		}
-		c.ServeJSON()
+		errMsg = fmt.Sprintf("controller not found, path: %s, method: %s", path, upperMethod)
+		log.Errorf(errMsg)
+		handleErrResponse(c, nil, errMsg)
 		return
 	}
 
@@ -110,21 +141,56 @@ func (c *PluginApiDispatchController) Post() {
 	if upperMethod == http.MethodGet {
 		newRequest.URL.RawQuery = parsedURL.RawQuery
 	} else if upperMethod == http.MethodPost {
-		newRequest.Header.Set("Content-Type", "application/json")
-		newRequest.Body = io.NopCloser(bytes.NewReader(param.Data))
-		newRequest.ContentLength = int64(len(param.Data))
-	} else {
-		log.Errorf("dispatch method not supported, method: %s\n", upperMethod)
-		c.Data["json"] = &PluginApiDispatchResponse{
-			BaseResponse: &BaseResponse{
-				Result:  false,
-				Message: fmt.Sprintf("dispatch method not supported, method: %s\n", upperMethod),
-			},
-			Data: nil,
+
+		if strings.HasPrefix(contentType, "multipart/form-data") {
+			var buffer bytes.Buffer
+			writer := multipart.NewWriter(&buffer)
+
+			if len(param.Data) > 0 {
+				_ = writer.WriteField("data", string(param.Data))
+			}
+
+			for _, fileHeader := range param.File {
+				file, err := fileHeader.Open()
+				if err != nil {
+					errMsg = "open file fail"
+					log.Errorf("%s, %v", errMsg, err)
+					handleErrResponse(c, err, errMsg)
+					return
+				}
+				part, err := writer.CreateFormFile("file", fileHeader.Filename)
+				if err != nil {
+					errMsg = "create form file fail"
+					log.Errorf("%s, %v", errMsg, err)
+					handleErrResponse(c, err, errMsg)
+					return
+				}
+
+				if _, err := io.Copy(part, file); err != nil {
+					errMsg = "copy file fail"
+					log.Errorf("%s, %v", errMsg, err)
+					handleErrResponse(c, err, errMsg)
+					return
+				}
+
+				if err := writer.Close(); err != nil {
+					errMsg = "close form file fail"
+					log.Errorf("%s, %v", errMsg, err)
+					handleErrResponse(c, err, errMsg)
+					return
+				}
+				newRequest.Body = io.NopCloser(&buffer)
+			}
+		} else {
+			newRequest.Body = io.NopCloser(bytes.NewReader(param.Data))
+			newRequest.ContentLength = int64(len(param.Data))
 		}
-		c.ServeJSON()
+
+	} else {
+		errMsg = fmt.Sprintf("dispatch method not supported, method: %s\n", upperMethod)
+		log.Errorf(errMsg)
+		handleErrResponse(c, err, errMsg)
 		return
 	}
-
 	web.BeeApp.Handlers.ServeHTTP(c.Ctx.ResponseWriter, newRequest)
 }
